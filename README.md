9170e2e9d31e75f3699859fed308eb33d452ab3e
working-ash0927 <103326981+working-ash0927@users.noreply.github.com>
2023-06-15 17:04:56 +0900

    change readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ping-asset
- sensu-go에서 활용 가능한 ping 에셋 코드
+sensu-go에서 활용 가능한 ping 에셋 코드
+
+https://github.com/go-ping/ping 코드를 참조하여 개발됨.
---
+++ ping.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+	"flag"
+	"github.com/go-ping/ping"
+)
+
+
+func getunixtime() int64 {
+	loc, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		panic(err)
+	}
+	now := time.Now()
+	t := now.In(loc)
+	a := t.UnixNano() / 1000000
+	return a
+}
+
+func getIPAddr() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println("# Failed to get IP address:", err)
+		return "failed to get IP of Agent"
+	}
+
+	for _, addr := range addrs {
+		ipNet, ok := addr.(*net.IPNet)
+		if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+			return ipNet.IP.String()
+		}
+	}
+
+	return "# failed to get IP of Agent"
+}
+
+func sendPing(addr string) {
+	sensu_backend := getIPAddr()
+	target := addr
+	nowtime := getunixtime()
+	
+	// Pinger 생성
+	pinger, err := ping.NewPinger(target)  // 대상서버 IP 입력
+	if err != nil {
+		fmt.Printf("ping_test{sensu_backend=\"%s\", target=\"%s\"} %d %d\n", sensu_backend, target, 0, nowtime)
+		fmt.Printf("# failed to make new pinger: %s\n", err.Error())
+		return
+	}
+
+	// PING 설정
+	pinger.Count = 1             // 1회 PING 전송
+	pinger.SetPrivileged(true)   // ICMP 대신 UDP 사용
+	pinger.Timeout = 100 * time.Millisecond // 타임아웃 0.1초로 설정
+
+	// 결과 처리 함수 등록
+	pinger.OnRecv = func(pkt *ping.Packet) {
+		fmt.Printf("# received: %d bytes, time: %v\n", pkt.Nbytes, pkt.Rtt)
+	}
+
+	pinger.OnFinish = func(stats *ping.Statistics) {
+		fmt.Printf("# %d packets transmitted, %d received, %.2f%% packet loss, average RTT: %v\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss, stats.AvgRtt)
+
+		if stats.PacketsRecv == 0 {
+			fmt.Printf("ping_test{sensu_backend=\"%s\", target=\"%s\"} %d %d\n", sensu_backend, target, 0, nowtime)
+			fmt.Println("# ping test failed or timed out")
+		} else{
+			fmt.Printf("ping_test{sensu_backend=\"%s\", target=\"%s\"} %d %d\n", sensu_backend, target, 1, nowtime)
+			fmt.Println("# ping test succeed")
+		}
+	}
+
+	// PING 실행
+	fmt.Printf("# ping test start: %s\n", pinger.Addr())
+	err = pinger.Run()
+	if err != nil {
+		fmt.Println("# ping did not run")
+		return
+	}
+}
+
+func main() {
+	var addr string
+
+	// command 상의 변수 할당
+	flag.StringVar( &addr, "addr", "", "AWS region") // Ping을 보내려는 서버의 IP(즉, Sensu backend)
+
+	flag.Parse()
+
+	sendPing(addr)
+}
